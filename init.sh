71e1a7944827327286e816427143dadb1e3088f9
Rossen Georgiev <zoorty@gmail.com>
2015-04-17 18:51:13 +0100

    better init.sh + gen secrey_key

--- init.sh
+++ init.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-virtualenv env
+# create virtual py enviroment and activate
+virtualenv --clear env
 source ./env/bin/activate
+
+# install deps
 pip install -r requirements.txt
+
+# generate secret_key
+python -c "from random import SystemRandom; r=SystemRandom(); print ''.join([r.choice('abcdefghipqrstuvwxyz0123456789@#$%^&*(-_=+)') for i in range(50)])" > secret_key.txt
+
